46852244525794c2e633638e62d23aee0de2026b
ElhamFadel <elham2000Fadel@gmail.com>
2020-09-29 03:58:34 +0300

    dom event click & input

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Here you can access the colors variable
+// have fun
+
+const butt = document.querySelector("body > div > div > div.control > button");
+const targt = document.querySelector("#pallete");
+const inpt = document.querySelector("#color");
+butt.addEventListener('click',change);
+
+inpt.addEventListener('input',change);
+
+function change(e){
+    if(e==="change"){
+    let numRand =Math.floor( Math.random()*(colors.length));
+    targt.style.backgroundColor = colors[numRand];
+    inpt.setAttribute("value",colors[numRand]);
+    }else{
+        var RegExp = /^#[0-9A-F]{6}$/i; //مش مفهومة منيح بس بحثت عنها 
+        if(RegExp.test(inpt.value)){
+        targt.style.backgroundColor =inpt.value  ;
+        inpt.style.color = inpt.value;
+    }
+
+    }
+
+}
