b0bf82d0eb6ea989d56eb60f780d98e56c688e80
ElhamFadel <elham2000Fadel@gmail.com>
2020-10-03 17:07:22 +0300

    The last adjustments

--- js/index.js
+++ js/index.js
@@ -1,28 +1,39 @@
-// Here you can access the colors variable
-// have fun
-
 const butt = document.querySelector("body > div > div > div.control > button");
-const targt = document.querySelector("#pallete");
-const inpt = document.querySelector("#color");
+const pallete = document.querySelector("#pallete");
+const inpt_color = document.querySelector("#color");
 butt.addEventListener('click',change);
-
-inpt.addEventListener('input',change);
-
+inpt_color.addEventListener('input',change);
 function change(e){
-
     if(e.type=="click"){
     let numRand =Math.floor( Math.random()*(colors.length));
-    targt.style.backgroundColor = colors[numRand];
-    inpt.value=colors[numRand];
-   
-    
+    pallete.style.backgroundColor = colors[numRand];
+    inpt_color.value=colors[numRand];
     }else{
         var RegExp = /^#[0-9A-F]{6||3}$/i; //مش مفهومة منيح بس بحثت عنها 
-        if(RegExp.test(inpt.value)){
-        targt.style.backgroundColor =inpt.value  ;
-        
+        if(RegExp.test(inpt_color.value)){
+            pallete.style.backgroundColor =inpt_color.value  ;
+    }}
+    inpt_color.style.color = inpt_color.value;
+}
+///change color
+let nIntervId;
+const crazy = document.querySelector("#crazy");
+function changeColor() {
+    if (nIntervId) {
+      clearInterval(nIntervId);
     }
-    }
-    inpt.style.color = inpt.value;
-
+    nIntervId = setInterval(()=>{
+        let numRand =Math.floor( Math.random()*(colors.length));
+    crazy.style.backgroundColor = colors[numRand];
+    crazy.textContent=colors[numRand];
+    }, 50);
+  }
+ crazy.addEventListener("click",changeColor);
+ crazy.addEventListener("dblclick",stopTextColor);
+ function stopTextColor() {
+    clearInterval(nIntervId);
+    let color =  crazy.textContent;
+   pallete.style.backgroundColor = color;
+    inpt_color.value=color;
+    crazy.textContent="Play";
 }
